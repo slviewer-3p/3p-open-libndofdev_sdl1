271ff7b6cbd4ef14f57b1a3c12de8495220d78ad
Anchor <none@none>
2017-05-26 14:38:36 -0800

    fix build script

--- build-cmd.sh
+++ build-cmd.sh
@@ -30,13 +30,13 @@ stage="$(pwd)"
 
 # load autobuild provided shell functions and variables
 "$autobuild" --quiet source_environment > "$build_log_dir/source_environment"
-begin_section "dump source environment commands"
-cat "$build_log_dir/source_environment"
-end_section "dump source environment commands"
 
-begin_section "execute source environment commands"
+cat "$build_log_dir/source_environment"
+
+
+
 . "$build_log_dir/source_environment"
-end_section "execute source environment commands"
+
 
 build=${AUTOBUILD_BUILD_ID:=0}
 echo "${VERSION}.${build}" > "${stage}/VERSION.txt"
