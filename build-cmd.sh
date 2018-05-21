827fa7a849940d75e88488cbd9c1449f7227b26a
Nicky <sl.nicky.ml@googlemail.com>
2018-05-21 18:59:07 +0200

    Update to new SDL 3P. While at it clean up the mess of building inside the source tree.

--- build-cmd.sh
+++ build-cmd.sh
@@ -51,5 +51,7 @@ case "$AUTOBUILD_PLATFORM" in
             -DWORD_SIZE:STRING=$AUTOBUILD_ADDRSIZE \
             -DCMAKE_BUILD_TYPE:STRING=Release
         make
+	cp -a ../libndofdev/include include
+	cp -a ../libndofdev/LICENSES LICENSES
     ;;
 esac
