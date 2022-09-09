92577ad48cbec5e2733dc4c242a8ad48aadb3b67
Nicky <nicky.dasmijn@posteo.nl>
2022-09-09 16:06:25 +0200

    Separate SDL1 version, so it can be installed side by side with the SDL2 version.

--- build-cmd.sh
+++ build-cmd.sh
@@ -51,7 +51,10 @@ case "$AUTOBUILD_PLATFORM" in
             -DWORD_SIZE:STRING=$AUTOBUILD_ADDRSIZE \
             -DCMAKE_BUILD_TYPE:STRING=Release
         make
-	cp -a ../libndofdev/include include
-	cp -a ../libndofdev/LICENSES LICENSES
+		mkdir -p include/SDL/
+		cp -a ../libndofdev/include/* include/SDL/
+
+		mkdir -p LICENSES/
+		cp -a ../libndofdev/LICENSES/libndofdev.txt LICENSES/libndofdev_sdl1.txt
     ;;
 esac
